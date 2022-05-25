e032431e0cfcd9f6fe55399a9b3fb94d0f1518b2
Lewis Heath <email@lewisheath.co.uk>
2022-05-26 00:12:28 +0100

    hover on squares works

--- server.js
+++ server.js
@@ -16,9 +16,62 @@ server.listen(process.env.PORT || 4000);
 io = socketIO(server);
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+    console.log('a user connected');
+
+    //emit a message to all clients
+    io.emit('colour-change', {colour: cycleColour()});
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        io.emit('colour-change', {colour: cycleColour()});
+    });
+
+    socket.on("button-click", function(data) {
+        console.log("ID of " + socket.id + " button clicked");
+        //get the id of the socket 
+        let socketId = socket.id;
+        io.emit("message", {id: socketId, message: data.message});
+        io.emit("colour-change", {colour: cycleColour()});
+    });
+
+    socket.on("square-hover", function({square}) {
+        console.log("ID of " + socket.id + " square hovered");
+        io.emit("square-hover", {square: square});
+    });
+
+    socket.on("square-unhover", function({square}) {
+        console.log("ID of " + socket.id + " square hovered");
+        io.emit("square-unhover", {square: square});
+    });
 
 });
+
+//GLOBAL VARIABLES FOR FUNCTIONS
+
+let colours = [
+    '#00ffec',
+    '#3dfc70',
+    '#ffa82c',
+    '#ff0081',
+    '#a300ff',
+]
+
+let currentColour = 0;
+
+function generateRandomColour(){
+    let letters = '0123456789ABCDEF';
+    let colour = '#';
+    for (let i = 0; i < 6; i++) {
+      colour += letters[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+}
+
+function cycleColour(){
+    currentColour++;
+    if(currentColour > colours.length - 1){
+        currentColour = 0;
+    }
+    console.log(colours[currentColour]);
+    return colours[currentColour];
+}
